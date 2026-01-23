451613eb46a4a4b1401c8e5d2c1c1512d487bb4b
UnbreakableMJ <34196588+UnbreakableMJ@users.noreply.github.com>
2026-01-23 12:49:07 +0300

    Added Steelbore fonts to theme.nix

--- Lattice/modules/core/theme.nix
+++ Lattice/modules/core/theme.nix
@@ -11,7 +11,32 @@ in
 {
   # Fonts
   fonts.packages = with pkgs; [
+    # NixOS standard packages
+    orbitron
+    jetbrains-mono
+    cascadia-code
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+
+    # Custom / Missing fonts
+    (fetchzip {
+      url = "https://font.download/dl/font/future-earth.zip";
+      hash = "sha256-025plp4kn6xria0j1k8sky729kk9p41d6j8rczzsdvj1a18jf2yz";
+      stripRoot = false;
+    })
+
+    (stdenv.mkDerivation {
+      pname = "share-tech-mono";
+      version = "1.0";
+      src = fetchurl {
+        url = "https://github.com/google/fonts/raw/main/ofl/sharetechmono/ShareTechMono-Regular.ttf";
+        hash = "sha256-0xr6ffvbx8516rxb5h2767fzfgp079bkgxf0b7r9m0hlfkwb3slw";
+      };
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/share/fonts/truetype
+        cp $src $out/share/fonts/truetype/ShareTechMono-Regular.ttf
+      '';
+    })
   ];
 
   # Styling hints (for apps that support it)
